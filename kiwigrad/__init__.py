c5f9268ef3afa489fa80f6184496b2913a343d31
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-04-03 16:21:48 +0200

    fixed cython import bug

--- kiwigrad/__init__.py
+++ kiwigrad/__init__.py
@@ -2,7 +2,8 @@ from .nn import (Neuron, Layer, MLP)
 from .rnn import (RNNNeuron, RNNLayer, RNN)
 from .graph import (draw_dot)
 from .engine import Value
-import pyximport; pyximport.install(language_level=3)
+import pyximport
+pyximport.install(language_level=3)
 from .primes import primes
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
