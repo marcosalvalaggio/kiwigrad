244a07c934a7711f5670f35841cebb6da1306ac1
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-04-08 18:35:57 +0200

    wip

--- kiwigrad/__init__.py
+++ kiwigrad/__init__.py
@@ -1,9 +1,11 @@
 from .graph import draw_dot
 import pyximport
+import os
 pyximport.install(language_level=3)
+os.environ['CYTHON_WARNINGS'] = 'none'
 from .engine import Value
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 __all__ = [
     "Value",
