87f905582f49940110615a27c600197d23c8f005
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-04-08 18:27:32 +0200

    wip

--- kiwigrad/__init__.py
+++ kiwigrad/__init__.py
@@ -1,9 +1,9 @@
 from .graph import draw_dot
 import pyximport
-pyximport.install(language_level=3)
+pyximport.install(language_level=3, setup_args={'options': {'build_ext': {'--no-cython-warnings': True}}})
 from .engine import Value
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 __all__ = [
     "Value",
