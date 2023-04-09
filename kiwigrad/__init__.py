e724822a292085d00fb2f38537f4c0707329b82b
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-04-09 11:10:10 +0200

    wip

--- kiwigrad/__init__.py
+++ kiwigrad/__init__.py
@@ -1,12 +1,12 @@
 from .graph import draw_dot
 import os
-os.environ['CYTHON_FLAGS'] = '--directive=warn.implicit_fallthrough=False'
+os.environ['CYTHON_FLAGS'] = '--warn=unused-fallthrough'
 import pyximport
 pyximport.install(language_level=3)
 from .engine import Value
 from .nn import (Neuron, Layer, MLP)
 
-__version__ = "0.12"
+__version__ = "0.13"
 
 __all__ = [
     "Value",
