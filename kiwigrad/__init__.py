e46d6c5d45c8f442647f0b417d701095e0fe1fca
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-04-09 11:00:44 +0200

    wip

--- kiwigrad/__init__.py
+++ kiwigrad/__init__.py
@@ -5,10 +5,11 @@ import pyximport
 pyximport.install()
 os.environ['CYTHON_WARNINGS'] = 'none'
 os.environ['CYTHON_WARN_UNUSED_FALLTHROUGH'] = 'false'
+os.environ['CYTHON_FLAGS'] = '-Wno-unused-fallthrough'
 from .engine import Value
 from .nn import (Neuron, Layer, MLP)
 
-__version__ = "0.0.9"
+__version__ = "0.1"
 
 __all__ = [
     "Value",
