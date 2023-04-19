3f5d370c3d5e2465dd7eec2b39f497492741499b
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-04-19 22:51:04 +0200

    wip new version

--- kiwigrad/__init__.py
+++ kiwigrad/__init__.py
@@ -1,13 +1,17 @@
 from .graph import draw_dot
 from .engine import Value
-from .nn import (Neuron, Layer, MLP)
+from .neurons import Neuron, RNN1Neuron
+from .layers import Layer, RNN1Layer
+from .nn import MLP
 
-__version__ = "0.17"
+__version__ = "0.18"
 
 __all__ = [
     "Value",
     "draw_dot",
     "Neuron",
+    "RNN1Neuron",
     "Layer",
-    "MLP"
+    "RNN1Layer",
+    "MLP",
 ]
