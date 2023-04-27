0df6d52097fa2a1bf41b045e1a74b0b312995316
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-04-27 15:54:54 +0200

    wip

--- test/test.py
+++ test/test.py
@@ -5,6 +5,7 @@ from sklearn.datasets import make_moons
 import numpy as np
 
 X, y = make_moons(n_samples=100, noise=0.1)
+inputs = [list(map(Value, xrow)) for xrow in X]
 print(X.shape, type(X))
 print(y.shape, type(y))
 
@@ -19,26 +20,4 @@ class PotNet(MLP):
 
 model = PotNet()
 
-def accuracy_val(model, X ,y_true):
-    y_pred = np.array([model(X[i]).sigmoid().data for i in range(X.shape[0])]).round()
-    correct_results = np.sum(y_pred == y_true)
-    acc = correct_results/y_true.shape[0]
-    acc = np.round(acc * 100)
-    return acc
-
-epochs = 100 
-for epoch in range(epochs): 
-    for i in range(X.shape[0]):
-        output = model(X[i]).sigmoid()
-        neg_output = 1-output
-        target = y[i]
-        bce_loss = -(target * output.log() + (1 - target) * neg_output.log())
-        bce_loss.backward()
-        for p in model.parameters():
-            p.data += -0.001 * p.grad 
-        model.zero_grad()
-    acc = accuracy_val(model=model, X=X, y_true=y)
-    if epoch%10 == 0:
-        print(f"epoch: {epoch} |", f"loss: {bce_loss.data:.2f} |", f"accuracy: {acc}")
-
-model.save()
+print(model(input[0]))
